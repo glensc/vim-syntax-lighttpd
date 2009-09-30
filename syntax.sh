c010bb138667ddbf81ddebf92904e0492b897efc
Elan Ruusamäe <glen@pld-linux.org>
2009-09-30 12:14:10 +0000

    - indent
    Changed files:
        syntax.sh -> 1.3

--- syntax.sh
+++ syntax.sh
@@ -8,6 +8,13 @@
 set -e
 for a in configfile.c mod_*.c; do
 	[ "$a" = "mod_skeleton.c" ] && continue
-#	echo "# $a"
-	cpp -DHAVE_LSTAT -E $a 2>/dev/null | sed -n '/static.*\(config_insert\|set_defaults\|log_access_open\)(/,/^}/{/config_values_t cv/,/};/{/T_CONFIG_DEPRECATED/d;/"\([^"]\+\)"/s/.*"\([^"]\+\)".*/\t\\ \1/p}}'
+	echo >&2 "# $a"
+	cpp -DHAVE_LSTAT -E $a 2>/dev/null | sed -n '
+	/static.*\(config_insert\|set_defaults\|log_access_open\)(/,/^}/{
+		/config_values_t cv/,/};/{
+			/T_CONFIG_DEPRECATED/d
+			/"\([^"]\+\)"/s/.*"\([^"]\+\)".*/\t\\ \1/p
+		}
+	}
+	'
 done | sort -u | grep -v not-working
